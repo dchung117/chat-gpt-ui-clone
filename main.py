4e1df76f46ec743f8c87c137139916aaadc79f3d
dchung117 <dchung0330@gmail.com>
2023-10-10 23:13:06 -0400

    send user messages to openai api for chat completion.

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ from dotenv import load_dotenv
 import chainlit as cl
 import openai
 
+MODEL = "gpt-4"
+SYSTEM_PROMPT = "You are a helpful assistant."
+TEMPERATURE = 1
 load_dotenv()
 
 @cl.on_message
-async def main(msg: str):
-    await cl.Message(content=msg).send()
+async def main(msg: str) -> None:
+    """
+    Send Chat-GPT response back to user based on message.
+
+    Args
+    ----
+        msg: str
+            User message to chatbot.
+
+    Return
+    ------
+        None
+    """
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages = [
+            {"role": "assistant", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": msg}
+        ],
+        temperature=TEMPERATURE,
+    )
+    await cl.Message(content=response["choices"][0]["message"]["content"]).send()
